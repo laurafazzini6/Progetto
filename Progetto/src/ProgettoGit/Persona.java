cadcf7ebfa325226e11fda15133fdc47291c10fd
Laura Fazzini <Laura Fazzini@DESKTOP-VECCO26.homenet.telecomitalia.it>
2020-11-21 16:30:51 +0100

    getter e setter

--- Progetto/src/ProgettoGit/Persona.java
+++ Progetto/src/ProgettoGit/Persona.java
@@ -8,6 +8,23 @@ public Persona(String nome, String cognome) {
 	super();
 	this.nome = nome;
 	this.cognome = cognome;
+	
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public String getCognome() {
+	return cognome;
+}
+
+public void setCognome(String cognome) {
+	this.cognome = cognome;
 }
 
 
